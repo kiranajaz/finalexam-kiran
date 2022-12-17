6d096e08e4fd9203bdb0464a62216f17f16a93da
kiranajaz <112897631+kiranajaz@users.noreply.github.com>
2022-12-17 02:58:01 -0500

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Product } from '../products';
 
+
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
