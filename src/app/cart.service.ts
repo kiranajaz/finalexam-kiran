839956e11981b7ec47b383e8d4bcf0c756a4c11c
kiranajaz <112897631+kiranajaz@users.noreply.github.com>
2022-12-17 04:14:27 -0500

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -7,17 +7,15 @@ import { Injectable } from '@angular/core';
 export class CartService {
   items: Product[] = [];
 
-  addToCart(product: Product) {
+   addToCart(product: Product) {
     this.items.push(product);
-  }
+}
 
   getItems() {
     return this.items;
-  }
+}
 
   clearCart() {
     this.items = [];
     return this.items; 
-
-  
 }
