133b2045483826be2bb1910b2f51c990ec859ddb
kiranajaz <112897631+kiranajaz@users.noreply.github.com>
2022-12-17 05:26:47 -0500

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -5,17 +5,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CartService {
+
   items: Product[] = [];
 
-   addToCart(product: Product) {
+addToCart(product: Product) {
     this.items.push(product);
-}
+  }
 
   getItems() {
     return this.items;
-}
+  }
 
   clearCart() {
     this.items = [];
-    return this.items; 
+    return this.items;
+  }
+
+  constructor() { }
 }
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -19,6 +19,6 @@ ngOnInit() {
   const routeParams = this.route.snapshot.paramMap;
   const productIdFromRoute = Number(routeParams.get('productId'));
     
-// Find the product that correspond with the id provided in route.
-this.product = products.find(product => product.id === productIdFromRoute);
+ // Find the product that correspond with the id provided in route.
+ this.product = products.find(product => product.id === productIdFromRoute);
 }
