0b8490bcb33d912afaf8a4b67c1d0ea6f4a53ef1
kiranajaz <112897631+kiranajaz@users.noreply.github.com>
2022-12-17 05:53:52 -0500

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -14,7 +14,7 @@ export class ProductDetailsComponent implements OnInit {
   addToCart(product: Product) {
     this.cartService.addToCart(product);
     window.alert('Your product has been added to the cart!');
-}
+  }
 
   product: Product | undefined;
 
@@ -24,15 +24,13 @@ export class ProductDetailsComponent implements OnInit {
     private cartService: CartService
 
   ) { }
-   
 
-
-ngOnInit() 
+ngOnInit( ) 
 { 
   // First get the product id from the current route.
   const routeParams = this.route.snapshot.paramMap;
   const productIdFromRoute = Number(routeParams.get('productId'));
-    
+   
  // Find the product that correspond with the id provided in route.
- this.product = products.find(product => product.id === productIdFromRoute)
-};
+ this.product = products.find(product => product.id === productIdFromRoute);
+}
